7e2117fcc9e896400aa785fe9349d5e71ee8a82a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-09-03 15:12:25 +0600

    update opertion in server side is done

--- index.js
+++ index.js
@@ -53,6 +53,31 @@ async function run() {
 
         })
 
+        // update operation same as delete operation
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const user = await userCollection.findOne(query)
+            res.send(user)
+        })
+
+        // put operation for update data in backend
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log(id, user);
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    name: user.name,
+                    email: user.email
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedUser, options)
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
